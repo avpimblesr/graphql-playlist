2a6c38a038420a49beb2c78ab886b33561882761
avpimblesr <allan.pimble@gmail.com>
2021-01-17 23:41:43 -0500

    Initial commit retried

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// connect to the mongoDB database
+mongoose.connect('mongodb://localhost:27017/gql-ninja', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database')
+})
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+)
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000')
+})
